be47463af19feb336cb4662d7d4024071d3ad265
Grace Han <ohlookitsgrace@gmail.com>
2018-10-30 22:54:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+//1. Get the element
+const main = document.querySelector
+  ('main')
+
+//2. Add an event listener
+function onClick() {
+  alert('i was clicked')
+}
+
+main.addEventListener('click', onClick)
+
+function onKeyDown(event) {
+  console.log(event)
+  if (event.which === 74) {
+    event.preventDefault()
+  }
+}
+
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', onKeyDown)
+
+const divs = document.querySelectorAll('div')
+console.log(divs)
+
+divs[3].addEventListener('click', onDivClick)
+
+function onDivClick(event) {
+  event.stopPropagation()
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+}
+
+//for (let div of divs) {
+//  div.addEventListener('click', onDivClick)
+//}
